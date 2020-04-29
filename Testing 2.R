d62c7f4bb19d4d798e708f0caa35a00a8285927f
reudaha <rdha@lakeheadu.ca>
2020-04-29 16:57:57 -0600

    Initial COmmit of Testing 2.

---
+++ Testing 2.R
@@ -0,0 +1,13 @@
+Hello,
+
+I am writing a letter here to see if I can push this file onto GitHub without any major issues arising. 
+
+Please add a comment here if you can see this. 
+
+Reudaha
+
+1+1
+
+
+print("This file ")
+print("HI Hi Hi")
